e44f9c9cad8aa12f1a9b57ba350aeb4b16feadf2
Thercístenes <thercistenes1@outlook.com>
2023-05-18 15:46:31 -0300

    9 - Crie uma função para selecionar uma cor na paleta de cores

--- script.js
+++ script.js
@@ -20,21 +20,18 @@
         localStorage.setItem('cor2', JSON.stringify(cor2.style.backgroundColor));
         localStorage.setItem('cor3', JSON.stringify(cor3.style.backgroundColor));
         localStorage.setItem('cor4', JSON.stringify(cor4.style.backgroundColor));  
-        
-        const recuperaDado = ()=> {
-            localStorage.getItem('cor2');
-            localStorage.getItem('cor3');
-            localStorage.getItem('cor4');
-        }
-        recuperaDado
     };
+        
+const recuperaDado = ()=> {
+    const cor2 = JSON.parse(localStorage.getItem('cor2'));
+    const cor3 = JSON.parse(localStorage.getItem('cor3'));
+    const cor4 = JSON.parse(localStorage.getItem('cor4'));
+}
+    recuperaDado()
     atualizaCorPaleta()
 
     const buttonRadomColor = document.querySelector('#button-random-color');
     buttonRadomColor.addEventListener('click', atualizaCorPaleta);
-
-    
-
 
 const pixelBoard = document.querySelector('#pixel-board');    
     for (let j = 0; j < 25; j++) {
@@ -43,6 +40,24 @@ const pixelBoard = document.querySelector('#pixel-board');
       pixel.style.backgroundColor = "white";
       pixelBoard.appendChild(pixel);
     }
+
+const corSelecionada = ()=> {
+    const paleta = document.querySelectorAll('.color');
+
+    for(let index = 0; index < paleta.length; index += 1) {
+        paleta[index].classList.remove('selected');
+    }
+
+    let corClicada = event.target;
+    corClicada.classList.add('selected');
+}
+
+const corInicialSelecionada = document.querySelector('.color');
+corInicialSelecionada.classList.add('selected');
+
+const paleta = document.querySelector('#color-palette');
+paleta.addEventListener('click', corSelecionada);
+
     
-    window.onload = ()=> {   
-    };
+window.onload = ()=> {   
+};
