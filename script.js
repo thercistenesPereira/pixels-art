05a7746713920d8a1efa94484336537afc898e26
Thercístenes <thercistenes1@outlook.com>
2023-05-18 11:22:59 -0300

    Refatoração do ponto 7

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = ()=> {
+
     const geraCorAleatoria = () => {
         let caracteresCores = '0123456789ABCDEF';
         let cor = '#';
@@ -19,32 +19,30 @@ window.onload = ()=> {
         
         localStorage.setItem('cor2', JSON.stringify(cor2.style.backgroundColor));
         localStorage.setItem('cor3', JSON.stringify(cor3.style.backgroundColor));
-        localStorage.setItem('cor4', JSON.stringify(cor4.style.backgroundColor));       
+        localStorage.setItem('cor4', JSON.stringify(cor4.style.backgroundColor));  
+        
+        const recuperaDado = ()=> {
+            localStorage.getItem('cor2');
+            localStorage.getItem('cor3');
+            localStorage.getItem('cor4');
+        }
+        recuperaDado
     };
     atualizaCorPaleta()
 
     const buttonRadomColor = document.querySelector('#button-random-color');
     buttonRadomColor.addEventListener('click', atualizaCorPaleta);
 
-    const initialize = ()=> {
-        let getCor2 = localStorage.getItem('cor2');    
-    }
-    initialize()
-}
-
-const pixelBoard = document.querySelector('#pixel-board');
-
-for (let index = 0; index < 5; index += 1) {
-    const linha = document.createElement("div");
-    linha.classList.add("linha");
     
-    for (let j = 0; j < 5; j++) {
+
+
+const pixelBoard = document.querySelector('#pixel-board');    
+    for (let j = 0; j < 25; j++) {
       const pixel = document.createElement("div");
       pixel.classList.add("pixel");
       pixel.style.backgroundColor = "white";
-      linha.appendChild(pixel);
+      pixelBoard.appendChild(pixel);
     }
     
-    pixelBoard.appendChild(linha);
-}
-
+    window.onload = ()=> {   
+    };
